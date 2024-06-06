255c1d31f8a7f33e9fa24ebdc9d2922280c672c9
Pranay Varada <52256079+pvar37@users.noreply.github.com>
2024-06-05 21:47:41 -0500

    Rearranging some files
    Insertable navbar, thumbnail functionality, resume update, etc.

---
+++ assets/js_pvp/header.js
@@ -0,0 +1,33 @@
+document.write('\
+\
+    <header id="header">\
+        <h1 id="logo"><a href="../pvp/">PVP PRODUCTIONS</a></h1>\
+        <nav id="nav">\
+            <ul>\
+                <li>\
+                    <a href="#">FEATURES</a>\
+                    <ul>\
+                        <li>\
+                            <a href="land-of-the-setting-sun.html">LAND OF THE SETTING SUN</a>\
+                            <ul>\
+                                <li><a href="land-of-the-setting-sun.html#Minamitorishima">I. MINAMITORISHIMA</a></li>\
+                                <li><a href="land-of-the-setting-sun.html#forgotten">II. THE FORGOTTEN LANDS</a></li>\
+                                <li><a href="land-of-the-setting-sun.html#ends">III. THE ENDS OF THE EARTH</a></li>\
+                            </ul>\
+                        </li>\
+                    </ul>\
+                </li>\
+                <li>\
+                    <a href="#">DISPATCHES</a>\
+                    <ul>\
+                        <li><a href="speedrunning-vienna.html">SPEEDRUNNING VIENNA</a></li>\
+                    </ul>\
+                </li>\
+                <li><a href="what.html">WHAT</a></li>\
+            </ul>\
+        </nav>\
+    </header>\
+');
+
+/* <li><a href="moldova-3-poland-2.html">MOLDOVA 3, POLAND 2</a></li>\
+<li><a href="predicting-euro-2024.html">PREDICTING EURO 2024</a></li>\ */

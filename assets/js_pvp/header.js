b9bbedb3b556f3940b5c9c7da015ad86a0d42ba3
Pranay Varada <52256079+pvar37@users.noreply.github.com>
2024-12-25 08:54:30 -0600

    Publishing "Far from home"

--- assets/js_pvp/header.js
+++ assets/js_pvp/header.js
@@ -7,6 +7,7 @@ document.write('\
                 <li>\
                     <a href="#">FEATURES</a>\
                     <ul>\
+                        <li><a href="moldova-3-poland-2.html">MOLDOVA 3, POLAND 2</a></li>\
                         <li>\
                             <a href="land-of-the-setting-sun.html">LAND OF THE SETTING SUN</a>\
                             <ul>\
@@ -15,12 +16,12 @@ document.write('\
                                 <li><a href="land-of-the-setting-sun.html#ends">III. THE ENDS OF THE EARTH</a></li>\
                             </ul>\
                         </li>\
-                        <li><a href="moldova-3-poland-2.html">MOLDOVA 3, POLAND 2</a></li>\
                     </ul>\
                 </li>\
                 <li>\
                     <a href="#">DISPATCHES</a>\
                     <ul>\
+                        <li><a href="far-from-home.html">FAR FROM HOME</a></li>\
                         <li><a href="recapping-euro-2024.html">RECAPPING EURO 2024</a></li>\
                         <li><a href="predicting-euro-2024.html">PREDICTING EURO 2024</a></li>\
                         <li><a href="speedrunning-vienna.html">SPEEDRUNNING VIENNA</a></li>\
